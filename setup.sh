0d40f056cb99145542b4ce1a0469db9bb3bc47a3
Benoit Masson <benoit.masson@ovhcloud.com>
2022-12-09 10:01:22 +0100

    Add custom user settings to workspaces
    + shellcheck

--- setup.sh
+++ setup.sh
@@ -3,16 +3,20 @@
 GITHUB_REPO_URL="git@github.com:gwleclerc/mob.sh.git"
 USERS=("gwendal" "benoit")
 
-DIRECTORY="$(basename "${GITHUB_REPO_URL}")"
+LOCAL_REPO_DIRECTORY="$(basename "${GITHUB_REPO_URL}")"
 
-if [[ ! -d "wd/$DIRECTORY" ]]; then
-    git clone --bare "$GITHUB_REPO_URL" "wd/$DIRECTORY"
+if [[ ! -d "wd/${LOCAL_REPO_DIRECTORY}" ]]; then
+    git clone --bare "${GITHUB_REPO_URL}" "wd/${LOCAL_REPO_DIRECTORY}"
 fi
 
-for subrepo in ${USERS[@]}; do
-    rm -rf "wd/$subrepo"
-    git clone "wd/$DIRECTORY" "wd/$subrepo"
-    pushd "wd/$subrepo" >& /dev/null
-    git remote add upstream "$GITHUB_REPO_URL"
-    popd >& /dev/null
+for subrepo in "${USERS[@]}"; do
+    rm -rf "wd/${subrepo}"
+    git clone "wd/${LOCAL_REPO_DIRECTORY}" "wd/${subrepo}"
+    pushd "wd/${subrepo}" >& /dev/null || continue
+    git remote add upstream "${GITHUB_REPO_URL}"
+    popd >& /dev/null || continue
+    if [[ -f ".vscode/${subrepo}.json" ]]; then
+        mkdir -p "wd/${subrepo}/.vscode"
+        cp -f ".vscode/${subrepo}.json" "wd/${subrepo}/.vscode/settings.json"
+    fi
 done
